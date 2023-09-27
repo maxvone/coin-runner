b03abbf3ebb54233ab22001b8eb73799b6016443
maxvone <maximvone@gmail.com>
2023-09-27 15:14:13 +0200

    add: slowdown effect

--- Assets/CodeBase/Pickupables/Effects/SlowDownSpeedEffect.cs
+++ Assets/CodeBase/Pickupables/Effects/SlowDownSpeedEffect.cs
@@ -3,14 +3,13 @@ using CodeBase.Services.Factories;
 
 namespace CodeBase.Pickupables.Effects
 {
-    public class BoostSpeedEffect : IEffectStrategy
+    public class SlowDownSpeedEffect : IEffectStrategy
     {
         private IMovementAreaDataHandler _movementAreaDataHandler;
-
         public void Execute()
         {
             _movementAreaDataHandler = AllServices.Container.Single<IMovementAreaDataHandler>();
-            _movementAreaDataHandler.MovementAreaMove.Speed *= 2;
+            _movementAreaDataHandler.MovementAreaMove.Speed /= 2;
         }
     }
 }
